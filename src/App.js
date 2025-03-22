11f2f9ccf9b03420e724694b1fff4e023af15961
juusando <hafid@zilmoo.com>
2025-03-23 02:25:20 +0900

    Temp - Done

--- src/App.js
+++ src/App.js
@@ -5,26 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <div className="icon-examples">
-          <SvgIcon name="search" className={"iconz"}/>
-          <SvgIcon name="username"/>
-          <SvgIcon name="cal" />
-          <SvgIcon name="users" />
-          <SvgIcon name="user_bb" />
-          <SvgIcon name="windows" />
+        <div className="icon-examples">
+          <SvgIcon name="zillapp" className={"logo"} />
         </div>
-        <p>
-          Edit src/App.js and save to reload.
-        </p>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        SKYKI
       </header>
     </div>
   );
