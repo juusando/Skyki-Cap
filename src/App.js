2716954d0f6de4f6b9be84eaffe11c2da4d00e77
juusando <hafid@zilmoo.com>
2026-01-29 22:00:44 +0900

    First Push - -Weather Page Temp

--- src/App.js
+++ src/App.js
@@ -5,10 +5,26 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="icon-examples">
-          <SvgIcon name="zillapp" className={"logo"} />
+      <div className="icon-examples">
+          <SvgIcon name="search" className={"iconz"}/>
+          <SvgIcon name="username"/>
+          <SvgIcon name="cal" />
+          <SvgIcon name="users" />
+          <SvgIcon name="user_bb" />
+          <SvgIcon name="windows" />
         </div>
-        SKYKI
+        <p>
+          Edit src/App.js and save to reload.
+        </p>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
